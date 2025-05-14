d49b2dcee05b5f804f61e94fbd63ee8d94dc5cbf
Alexey <30863409+Oberonru@users.noreply.github.com>
2025-05-14 15:58:13 +0600

    Fix Animator issues

--- Assets/Main/Scripts/Gameplay/Player/PlayerMove.cs
+++ Assets/Main/Scripts/Gameplay/Player/PlayerMove.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace Main.Scripts.Gameplay.Player
@@ -9,56 +8,6 @@ namespace Main.Scripts.Gameplay.Player
 
         private void Start()
         {
-            
         }
-
-        // public float jumpHeight = 2.0f;
-        // public float speed = 1;
-        // private CharacterController _controller;
-        // private Vector3 _velocity;
-        // private float _gravity = -9.81f;
-        // private float _verticalVelocity = 0;
-        //
-        //
-        // private void Start()
-        // {
-        //     _controller = GetComponent<CharacterController>();
-        // }
-        //
-        // private void Update()
-        // {
-        //     float horizontal = Input.GetAxis("Horizontal");
-        //
-        //     Vector3 move = new Vector3(0, 0, horizontal);
-        //     move = transform.TransformDirection(move);
-        //
-        //     _controller.Move(move * (speed * Time.deltaTime));
-        //     if (!_controller.isGrounded)
-        //     {
-        //         _verticalVelocity += _gravity * Time.deltaTime;
-        //     }
-        //     else if(_verticalVelocity < 0)
-        //     {
-        //         _verticalVelocity = 0;
-        //     }
-        //
-        //     move.y = _verticalVelocity;
-        //     
-        //     if (CanJump())
-        //     {
-        //         print("can jump");
-        //         Jump();
-        //     }
-        // }
-        //
-        // private void Jump()
-        // {
-        //     _velocity.y = Mathf.Sqrt(jumpHeight * -2f * _gravity);
-        // }
-        //
-        // private bool CanJump()
-        // {
-        //     return Input.GetKeyDown(KeyCode.Space) && _controller.isGrounded;
-        // }
     }
 }
