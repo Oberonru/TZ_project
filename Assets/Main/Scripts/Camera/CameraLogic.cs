c672831f9392a15dd9de58f2b2b32068439133c6
Alexey <30863409+Oberonru@users.noreply.github.com>
2025-05-18 15:10:58 +0600

    Edit

--- Assets/Main/Scripts/Camera/CameraLogic.cs
+++ Assets/Main/Scripts/Camera/CameraLogic.cs
@@ -4,33 +4,32 @@ namespace Main.Scripts.Camera
 {
     public class CameraLogic : MonoBehaviour
     {
-            public float RotationAngleX;
-            public int Distance;
-            public float OffsetY;
+        public float RotationAngleX;
+        public int Distance;
+        public float OffsetY;
 
-            [SerializeField] private Transform _following;
+        [SerializeField] private Transform _following;
 
-            private void LateUpdate()
-            {
-                if (_following == null)
-                    return;
-                Quaternion rotation = Quaternion.Euler(RotationAngleX, 0, 0);
-                Vector3 position = rotation * new Vector3(0, 0, -Distance) + FollowingPointPosition();
-                transform.rotation = rotation;
-                transform.position = position;
-                //transform.position = _following.transform.position;
-            }
+        private void LateUpdate()
+        {
+            if (_following == null)
+                return;
+            Quaternion rotation = Quaternion.Euler(RotationAngleX, 0, 0);
+            Vector3 position = rotation * new Vector3(0, 0, -Distance) + FollowingPointPosition();
+            transform.rotation = rotation;
+            transform.position = position;
+        }
 
-            public void Follow(GameObject following)
-            {
-                _following = following.transform;
-            }
+        public void Follow(GameObject following)
+        {
+            _following = following.transform;
+        }
 
-            private Vector3 FollowingPointPosition()
-            {
-                Vector3 followingPosition = _following.position;
-                followingPosition.y += OffsetY;
-                return followingPosition;
-            }
+        private Vector3 FollowingPointPosition()
+        {
+            Vector3 followingPosition = _following.position;
+            followingPosition.y += OffsetY;
+            return followingPosition;
         }
     }
+}
